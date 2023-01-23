468ebb30d7daabc21394d7868c5371a1dc5ad926
Vladimir <bigcorvet@gmail.com>
2023-01-23 18:58:17 +0200

    add def get_screenshot

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
 
-# Create your views here.
+from datetime import datetime
+from selenium import webdriver
+
+import base64
+import os
+import urllib.parse as urlparse
+
+def get_screenshot(request):
+    """Сделать снимок экрана и вернтуть файл png на основе url"""
+    
+

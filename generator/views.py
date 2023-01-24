07e65e838ef892a318b9177828f01c9921438cbe
Vladimir <bigcorvet@gmail.com>
2023-01-24 14:06:21 +0200

    update main function

--- generator/views.py
+++ generator/views.py
@@ -3,13 +3,45 @@ from django.http import HttpResponse
 from django.conf import settings
 
 from datetime import datetime
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 import base64
 import os
-import urllib.parse as urlparse
+import urllib.parse
 
 def get_screenshot(request):
     """Сделать снимок экрана и вернтуть файл png на основе url"""
+
+    width = 1024
+    height = 768
+
+    if request.method == 'POST' and 'url' in request.POST:
+        url = request.POST.get('url', '')
+        if url is not None and url != '':
+            save = False
+            base_url = '{0.scheme}://{0.netloc}'.format(urllib.parse.urlsplit(url))
+            domain = urllib.parse.urlsplit(url)[1]
+            params = urllib.parse.parse_qs(urllib.parse.urlparse(url).query)
+            if len(params) > 0:
+                if 'w' in params: width = int(params['w'][0])
+                if 'h' in params: height = int(params['h'][0])
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+            driver.get(url)
+            driver.set_window_size(width, height)
+
+            
+
+
+
+
+   
     
+
+
+
 
